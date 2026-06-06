2dc139e10ba8da68b165e2ffbeedf0c963d1fea0
payamshafiqullah-beep <payamshafiqullah@gmail.com>
2026-06-06 15:02:08 +0200

    Use redesigned homepage app entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
 import './index.css'
-import { LanguageProvider } from './contexts/LanguageContext'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import HowItWorks from './pages/HowItWorks'
-import Offer from './pages/Offer'
-import Gallery from './pages/Gallery'
-import FAQ from './pages/FAQ'
-import Contact from './pages/Contact'
-import Booking from './pages/Booking'
-import Privacy from './pages/Privacy'
-import Terms from './pages/Terms'
-
-function Layout({ children }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LanguageProvider>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/a-propos" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/comment-ca-marche" element={<HowItWorks />} />
-            <Route path="/offre-gratuite" element={<Offer />} />
-            <Route path="/galerie" element={<Gallery />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/reservation" element={<Booking />} />
-            <Route path="/politique-de-confidentialite" element={<Privacy />} />
-            <Route path="/conditions-generales" element={<Terms />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </LanguageProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
